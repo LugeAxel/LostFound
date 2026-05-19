625876408db0fe5260cbe343d1d13b16487c6cc9
LugeAxel <ahmadlutfialmuzakir@gmail.com>
2026-05-19 13:55:10 +0700

    FIX: BUG, AND WEBSOCKET

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,5 +2,5 @@ const apiUrl = (import.meta as any).env.VITE_API_URL;
 if (!apiUrl) {
   console.warn('⚠️ VITE_API_URL is not set. Using default: http://localhost:5005');
 }
-export const API_URL = apiUrl || 'http://localhost:5005';
-export const SOCKET_URL = apiUrl || 'http://localhost:5005';
+export const API_URL = apiUrl;
+export const SOCKET_URL = apiUrl;
