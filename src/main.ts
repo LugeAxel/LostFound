a12818891f53909ab135afa5d31dfe6f83b18508
LugeAxel <ahmadlutfialmuzakir@gmail.com>
2026-05-20 23:43:11 +0700

    Fix: hopefully

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createUnhead, headSymbol } from '@unhead/vue'
+import { createHead } from '@unhead/vue/client'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-const head = createUnhead()
 
-app.provide(headSymbol, head)
+app.use(createHead())
 app.use(router)
 
 app.mount('#app')
